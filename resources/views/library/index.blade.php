f209da4e949226c50f9791cc49a88546fbd8716c
mastjoe <josephnkwonta@gmail.com>
2019-09-28 22:47:33 +0100

    search form for library

--- resources/views/library/index.blade.php
+++ resources/views/library/index.blade.php
@@ -4,16 +4,40 @@
 
 @section('content')
     <div class="container-fluid">
-        @if (count($categories))
-            <div class="row">
-                
+        <div class="row">
+            <div class="col-md-9">
+                @if (count($categories))
+                    @include('library.search-form')
+                    <div class="row">
+                        @foreach ($categories as $category)
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <h5>0</h5>
+                                        <h6><small>Resources</small></h6>
+                                        <h3 class="font-weight-bold">{{ $category->name }}</h3>
+                                    </div>
+                                    <div class="card-footer text-center">
+                                        <a class="btn btn-primary px-5" href="">
+                                            view
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                @else
+                    <div class="text-center text-muted my-5">
+                        <h4 class="display-4">
+                            No resource category found
+                        </h4>
+                    </div>
+                @endif
             </div>
-        @else
-            <div class="text-center text-muted my-5">
-                <h4 class="display-4">
-                    No resource category found
-                </h4>
+            <div class="col-md-3">
+
             </div>
-        @endif
+        </div>
+        
     </div>
 @endsection
---
+++ resources/views/library/search-form.blade.php
@@ -0,0 +1,20 @@
+{{-- search form --}}
+<div class="row justify-content-center mb-3">
+    <div class="col-md-6">
+        <form method="GET">
+            <div class="input-group">
+                <input 
+                    type="search" 
+                    class="form-control bg-transparent"
+                    placeholder="Search categories"
+                    name="search"
+                >
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit">
+                        Search
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
